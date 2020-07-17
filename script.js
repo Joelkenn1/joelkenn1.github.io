01800922d8327acef72e9cba3aaeb2184b9fb979
Joelkenn1 <65873072+Joelkenn1@users.noreply.github.com>
2020-07-17 11:11:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function submit() {
         resultHEAD.innerHTML = "You got Labrador Retriever!";
         resultparagraph.innerHTML = " The Labrador Retriever is America's #1 dog breed. They are great family dogs, and are also very easy to train." +
         "Labs are entusiastic and require a lot of exercise like walking and swimming."; }
-     else if ( work === "A" ) {
+     else if ( work === "A" && weather === "B" && protection === "B") {
         resultHEAD.innerHTML = "You got Chiuahua!";
         resultparagraph.innerHTML = "The Chiuahua is the smallest breed of dog. Chiuahua's are very loyal to there owners. They often snap at strangers who may be threatening to them." +
         "Chiuahua's are confident and do reqiure training. Overall they are the perfect city pet.";}
@@ -26,11 +26,11 @@ function submit() {
     resultHEAD.innerHTML = "You got English Bulldog!";
     resultparagraph.innerHTML = "Bulldog's are sweet and gentle dogs. They are great family-pets, specifcally families containing small children. English Bulldog's love human attention." +
      "They are also low endurance and dont require much exercise. Bulldog's possess courage which can make them nice watchdogs too.";}
-    else if (protection === "A" && work === "A" || "B" ) {
+    else if (protection === "A") {
         resultHEAD.innerHTML = "You got Doberman Pinscher!";  
         resultparagraph.innerHTML = "Doberman Pinscher's are loyal and fearless dogs. Most may consider Doberman's as aggresive because of it's repuataion of being a war/police dog, however, in reality they are loving and caring." +
         "Doberman's only turn on their protective instincts when faced with danger. Through training, Doberman Pinscher's become dependable guard dogs.";}
-     else if (weather === "A" && work === "A" || "B") {
+     else if (weather === "A") {
     resultHEAD.innerHTML = "You got Bernese Moutain Dog!";
     resultparagraph.innerHTML = "The Bernese Moutain Dog is known for being strong and powerful. They are sweet and get along with everyone they come in contact with. Their thick, long coats allow them to strive in cold weather." +
     "The Bernese Moutain Dog is very versatile and is always up for a hard job.";}
